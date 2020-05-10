dd78fab088beb686c5b0a1e6a8d00c91e3292cef
Alfonso <vc.alfonso@hotmail.com>
2020-05-09 22:21:44 -0500

    configurations

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '1440px',
+    },
+    fontFamily: {
+      body: ['Karla', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'cyan-primary': 'hsl(179, 62%, 43%)',
+        'bright-yellow-primary': 'hsl(71, 73%, 54%)',
+        'light-gray-neutral': 'hsl(204, 43%, 93%)',
+        'grayish-blue-neutral': 'hsl(218, 22%, 67%)',
+        'cyan-secondary': '#4abebd',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
